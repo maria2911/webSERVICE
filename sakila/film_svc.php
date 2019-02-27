d4db4c06cb68b4d93d88c4be13c0dd7cc4250bb8
jumaidi <jumaidi10@gmail.com>
2019-02-27 10:07:45 +0800

    ver 1

--- sakila/film_svc.php
+++ sakila/film_svc.php
@@ -7,7 +7,7 @@ $server = 'localhost';
 $user = 'root';
 $pwd = '';
 $db = 'sakila';
-$port ='3307';
+$port =3307;
 $con = mysqli_connect($server, $user, $pwd, $db,$port);
 
 if(!$con)
@@ -19,9 +19,14 @@ else
 {
     $sql = "SELECT * FROM film LIMIT 0,20 ";
     $rs = mysqli_query($con,$sql);
+    $arr =[];
     while ($row = mysqli_fetch_array($rs))
     {
-        echo $row['title'].'<br>';
+       // echo $row['title'].'<br>';
+        $obj = new stdClass();
+        $obj->title = $row['title'];
+        $obj->descr = $row['description'];
+        $arr[] = $obj;
     }
 
 }
@@ -29,3 +34,7 @@ else
 // query ke DB
 
 // print data sbg JSON
+// {} atau [] - json_encode() vs json_decode()
+    $json =json_encode($arr);
+    header('Content-Type: application/json');
+    echo $json;
